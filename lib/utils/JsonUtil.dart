070fe4f4df5f6af1939781b404b1c747e75c7c04
chenlei <chen1994118@foxmail.com>
2023-08-24 11:01:47 +0800

    优化

--- lib/utils/JsonUtil.dart
+++ lib/utils/JsonUtil.dart
@@ -21,16 +21,18 @@ class JsonUtil {
 
   /// 接收Dio请求库返回的Response对象
   static void printRespond(Response response) {
-    Map httpLogMap = {};
-    httpLogMap.putIfAbsent("requestUrl", () => '${response.requestOptions.baseUrl}${response.requestOptions.path}');
-    httpLogMap.putIfAbsent("requestHeaders", () => response.headers.map);
-    httpLogMap.putIfAbsent("requestQueryParameters", () => response.requestOptions.queryParameters);
-    httpLogMap.putIfAbsent("requestData", () => response.requestOptions.data);
-    httpLogMap.putIfAbsent("requestExtra", () => response.extra);
-    httpLogMap.putIfAbsent("respondCode", () => response.statusCode);
-    httpLogMap.putIfAbsent("respondMessage", () => response.statusMessage);
-    httpLogMap.putIfAbsent("respondData", () => response.data);
+    Future((){
+      Map httpLogMap = {};
+      httpLogMap.putIfAbsent("requestUrl", () => '${response.requestOptions.baseUrl}${response.requestOptions.path}');
+      httpLogMap.putIfAbsent("requestHeaders", () => response.headers.map);
+      httpLogMap.putIfAbsent("requestQueryParameters", () => response.requestOptions.queryParameters);
+      httpLogMap.putIfAbsent("requestData", () => response.requestOptions.data);
+      httpLogMap.putIfAbsent("requestExtra", () => response.extra);
+      httpLogMap.putIfAbsent("respondCode", () => response.statusCode);
+      httpLogMap.putIfAbsent("respondMessage", () => response.statusMessage);
+      httpLogMap.putIfAbsent("respondData", () => response.data);
 
-    printJson(httpLogMap);
+      printJson(httpLogMap);
+    });
   }
 }
